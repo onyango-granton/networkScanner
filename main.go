9d968fa6ec331f6dd95477ac7ca5e8fee4f19f24
gonyango <grantononyango@gmail.com>
2024-05-07 11:15:10 +0300

    Veni

--- main.go
+++ main.go
@@ -33,10 +33,11 @@ func KnowMyIP() []string {
 		fmt.Println(err1.Error())
 	}
 
-	return obtainIP("output.txt")
+	return ObtainIP("output.txt")
 }
 
-func obtainIP(s string) []string {
+// ObtainIP takes in a file as string argument and return a list of IP Addresses
+func ObtainIP(s string) []string {
 	// Read contents of file passed as argument
 	out, _ := os.ReadFile(s)
 	splitS := strings.Split(string(out), "\n")
@@ -54,26 +55,21 @@ func obtainIP(s string) []string {
 			if strings.Contains(ch, "inet6") {
 				continue
 			}
+			// Filters out loopback address
 			if strings.Contains(ch, "127.0.0.1") {
 				continue
 			}
+
+			// Cleans up to obtain only the ip address and broadcast address
 			ch = strings.TrimLeft(ch, "inet ")
 			ch = strings.TrimRight(ch, " scope global dynamic noprefixroute wlp4s0")
-			// inet = append(inet, ch)
 
 			stringsSplit := strings.Split(ch, " ")
+			// Appending only the ip address with subnet mask
 			ipAddr = append(ipAddr, stringsSplit[0])
 
-			// for k, num := range stringsSplit[0]{
-
-			// }
 		}
 	}
-
-	// for _, ch := range ipAddr{
-	// 	re ,_ := strconv.Atoi(ch)
-	// 	fmt.Println(re)
-	// }
 
 	return ipAddr
 }
